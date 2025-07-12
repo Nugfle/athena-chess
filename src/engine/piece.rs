63c1e0b335481add6b0d99dc96eb93ec4ac7b2e3
Niklas <niklas.liesch@protonmail.com>
2025-07-12 08:41:21 +0200

    added Knight and Bishop 	modified:   src/engine/piece.rs

--- src/engine/piece.rs
+++ src/engine/piece.rs
@@ -67,9 +67,92 @@ impl Movable for Piece<Pawn> {
     }
 }
 
+impl Movable for Piece<Knight> {
+    fn targeted_squares(&self, position: Square, _: &Board) -> Vec<Square> {
+        let mut targeted = Vec::new();
+        if position.get_horizontal() <= 5 {
+            if position.get_vertical() <= 6 {
+                targeted.push(Square::new(position.get_horizontal() + 2, position.get_vertical() + 1).unwrap());
+            }
+            if position.get_vertical() >= 1 {
+                targeted.push(Square::new(position.get_horizontal() + 2, position.get_vertical() - 1).unwrap());
+            }
+        }
+        if position.get_horizontal() >= 2 {
+            if position.get_vertical() <= 6 {
+                targeted.push(Square::new(position.get_horizontal() - 2, position.get_vertical() + 1).unwrap());
+            }
+            if position.get_vertical() >= 1 {
+                targeted.push(Square::new(position.get_horizontal() - 2, position.get_vertical() - 1).unwrap());
+            }
+        }
+        if position.get_vertical() <= 5 {
+            if position.get_horizontal() <= 6 {
+                targeted.push(Square::new(position.get_horizontal() + 1, position.get_vertical() + 2).unwrap());
+            }
+            if position.get_horizontal() >= 1 {
+                targeted.push(Square::new(position.get_horizontal() - 1, position.get_vertical() + 2).unwrap());
+            }
+        }
+        if position.get_vertical() >= 2 {
+            if position.get_horizontal() <= 6 {
+                targeted.push(Square::new(position.get_horizontal() + 1, position.get_vertical() - 2).unwrap());
+            }
+            if position.get_horizontal() >= 1 {
+                targeted.push(Square::new(position.get_horizontal() - 1, position.get_vertical() - 2).unwrap());
+            }
+        }
+        targeted
+    }
+}
+
 impl Movable for Piece<Bishop> {
     fn targeted_squares(&self, position: Square, board: &Board) -> Vec<Square> {
         let mut targeted = Vec::new();
+        let mut blocked_neg_neg = false;
+        let mut blocked_neg_pos = false;
+        let mut blocked_pos_neg = false;
+        let mut blocked_pos_pos = false;
+        for i in position
+            .get_vertical()
+            .max(position.get_horizontal())
+            .max(position.get_vertical().abs_diff(7))
+            .max(position.get_horizontal().abs_diff(7))..0
+        {
+            if let Ok(occupied) = board.square_is_occupied((position.get_horizontal() - i, position.get_vertical() - i)) {
+                if !blocked_neg_neg {
+                    targeted.push(Square::new(position.get_horizontal() - i, position.get_vertical() - i).unwrap());
+                }
+                if occupied {
+                    blocked_neg_neg = true;
+                }
+            }
+            if let Ok(occupied) = board.square_is_occupied((position.get_horizontal() + i, position.get_vertical() - i)) {
+                if !blocked_pos_neg {
+                    targeted.push(Square::new(position.get_horizontal() + i, position.get_vertical() - i).unwrap());
+                }
+                if occupied {
+                    blocked_pos_neg = true;
+                }
+            }
+            if let Ok(occupied) = board.square_is_occupied((position.get_horizontal() - i, position.get_vertical() + i)) {
+                if !blocked_neg_pos {
+                    targeted.push(Square::new(position.get_horizontal() - i, position.get_vertical() + i).unwrap());
+                }
+                if occupied {
+                    blocked_neg_pos = true;
+                }
+            }
+            if let Ok(occupied) = board.square_is_occupied((position.get_horizontal() + i, position.get_vertical() + i)) {
+                if !blocked_pos_pos {
+                    targeted.push(Square::new(position.get_horizontal() + i, position.get_vertical() + i).unwrap());
+                }
+                if occupied {
+                    blocked_pos_pos = true;
+                }
+            }
+        }
+        targeted
     }
 }
 
